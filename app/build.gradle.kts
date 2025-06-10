f6371f38d520c8fd69dc77c164a00a5ba0eb64c3
Famanias <haratayo@gmail.com>
2025-06-10 16:36:30 +0800

    UI Changes
    Column display of translation result

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,8 @@ dependencies {
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(libs.volley)
+    implementation(libs.androidx.cardview)
+    implementation(libs.material)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
